6490b16722f662c69dce5433661a9af8bf748267
Aryan <aryan.k@cisit.in>
2025-01-11 17:49:39 +0530

    create: welcome card

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import AllPosts from "@/components/AllPosts";
 import CreatePost from "@/components/CreatePost";
+import Welcome from "@/components/Welcome";
 
 export default function Home() {
   return (
     <div className="m-5 w-full">
+      <Welcome />
       <CreatePost />
       <AllPosts />
     </div>
